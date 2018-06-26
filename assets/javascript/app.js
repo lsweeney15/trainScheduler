113a8060f571346343b8fcec79c8158417bff70e
Liam Sweeney <liamsweeney@liam-sweeneys-computer.local>
2018-06-25 22:26:00 -0400

    Api still not working

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,53 +1,55 @@
 // Initialize Firebase
-  var config = {
+var config = {
     apiKey: "AIzaSyBKKe02O8JZ0WHGObA1DGgI6gm5JArzzgQ",
     authDomain: "train-scheduler-c2046.firebaseapp.com",
     databaseURL: "https://train-scheduler-c2046.firebaseio.com",
     projectId: "train-scheduler-c2046",
     storageBucket: "",
     messagingSenderId: "203363900840"
-  };
-  firebase.initializeApp(config);
+};
+firebase.initializeApp(config);
 
-  var trainData = firebase.database();
+var trainData = firebase.database();
 
-  $("#addTrainBtn").on("click", function(){
-      var trainName = $("#trainNameInput").val().trim();
-      var destination = $("#destinationInput").val().trim();
-      var firstTrain = $("#firstTrainInput").val().trim(), "HH:mm").subtract(10, "years").format("X");
-      var frequency = $("#frequencyInput").val().trim();
+$("#addTrainBtn").on("click", function () {
+    var trainName = $("#trainNameInput").val().trim();
+    var destination = $("#destinationInput").val().trim();
+    var firstTrain = moment($("#firstTrainInput").val().trim(), "HH:mm").subtract(10, "years").format("X");
+    var frequency = $("#frequencyInput").val().trim();
 
-      var newTrain = {
-          name: trainName,
-          destination: destination;
-          firstTrain: firstTrain,
-          frequency: frequency
-      }
+    var newTrain = {
+        name: trainName,
+        destination: destination;
+        firstTrain: firstTrain,
+        frequency: frequency
+    }
 
-      trainData.ref().push(newTrain);
+    trainData.ref().push(newTrain);
 
-      alert("Train Added!");
+    alert("Train Added!");
 
-      $("#trainNameInput").val("");
-      $("#destinationInput").val("");
-      $("#firstTrainInput").val("");
-      $("#frequencyInput").val("");
+    $("#trainNameInput").val("");
+    $("#destinationInput").val("");
+    $("#firstTrainInput").val("");
+    $("#frequencyInput").val("");
 
-      return false;
+    return false;
 
-  })
+})
 
-trainDate.ref().on("child_added", function(snapshot){
-var name = snapshot.val().name;
-var destination = snapshot.val().destination;
-var frequency = snapshot.val().frequency;
-var firstTrain = snapshot.val().firstTrain;
+trainData.ref().on("child_added", function (snapshot) {
+    var name = snapshot.val().name;
+    var destination = snapshot.val().destination;
+    var frequency = snapshot.val().frequency;
+    var firstTrain = snapshot.val().firstTrain;
 
-var remainder = moment().diff(moment.unix(firstTrain), "minutes")%frequency;
-var minutes = frequency - remainder;
-var arrival = moment().add(minutes, "m").format("hh:mm A");
+    var remainder = moment().diff(moment.unix(firstTrain), "minutes") % frequency;
+    var minutes = frequency - remainder;
+    var arrival = moment().add(minutes, "m").format("hh:mm A");
 
-console.log(remainder);
-console.log(minutes);
-console.log(arrival);
+    console.log(remainder);
+    console.log(minutes);
+    console.log(arrival);
+
+    $("#trainTable > tBody").append("<tr><td>"+name+"</td><td>"+destination+"</td><td>"+frequency+"</td><td>"+arrival+"</td><td>"+minutes+"</td></tr>");
 })
