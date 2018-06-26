8b2cf9a5f3fbbc02f79a57ace63d441920627d5b
Liam Sweeney <liamsweeney@liam-sweeneys-computer.local>
2018-06-25 21:42:43 -0400

    initial commit

---
+++ assets/javascript/app.js
@@ -0,0 +1,53 @@
+// Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyBKKe02O8JZ0WHGObA1DGgI6gm5JArzzgQ",
+    authDomain: "train-scheduler-c2046.firebaseapp.com",
+    databaseURL: "https://train-scheduler-c2046.firebaseio.com",
+    projectId: "train-scheduler-c2046",
+    storageBucket: "",
+    messagingSenderId: "203363900840"
+  };
+  firebase.initializeApp(config);
+
+  var trainData = firebase.database();
+
+  $("#addTrainBtn").on("click", function(){
+      var trainName = $("#trainNameInput").val().trim();
+      var destination = $("#destinationInput").val().trim();
+      var firstTrain = $("#firstTrainInput").val().trim(), "HH:mm").subtract(10, "years").format("X");
+      var frequency = $("#frequencyInput").val().trim();
+
+      var newTrain = {
+          name: trainName,
+          destination: destination;
+          firstTrain: firstTrain,
+          frequency: frequency
+      }
+
+      trainData.ref().push(newTrain);
+
+      alert("Train Added!");
+
+      $("#trainNameInput").val("");
+      $("#destinationInput").val("");
+      $("#firstTrainInput").val("");
+      $("#frequencyInput").val("");
+
+      return false;
+
+  })
+
+trainDate.ref().on("child_added", function(snapshot){
+var name = snapshot.val().name;
+var destination = snapshot.val().destination;
+var frequency = snapshot.val().frequency;
+var firstTrain = snapshot.val().firstTrain;
+
+var remainder = moment().diff(moment.unix(firstTrain), "minutes")%frequency;
+var minutes = frequency - remainder;
+var arrival = moment().add(minutes, "m").format("hh:mm A");
+
+console.log(remainder);
+console.log(minutes);
+console.log(arrival);
+})
